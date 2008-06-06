dd0eda954c3f843f9ddecf59c55009a488b3fce0
peter <peter@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2008-06-06 08:20:41 +0000

    Call checkacl.  It is cosmetic, but allows the mail scripts and pre-commit scripts to do the right thing.  I'll redo this in script sometime.
    git-svn-id: https://svn.freebsd.org/base/svnadmin@179602 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- hooks/scripts/env.sh
+++ hooks/scripts/env.sh
@@ -4,3 +4,6 @@ PATH=/s/svn/base/hooks/scripts:/usr/bin:/bin:/usr/local/bin:/usr/sbin:/sbin:/usr
 export PATH
 cd /s/svn/base
 umask 002
+if [ -x /usr/local/bin/checkacl ]; then
+  eval `/usr/local/bin/checkacl`
+fi
