345ea2c7687781c84b9db26fea4b639671ce2a29
rdivacky <rdivacky@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2010-02-16 09:31:36 +0000

    Update clang to r96341.
    git-svn-id: https://svn.freebsd.org/base/vendor/clang/dist/test@203955 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- CodeGenObjC/debug-info-crash.m
+++ CodeGenObjC/debug-info-crash.m
@@ -1,6 +1,29 @@
-// RUN: %clang_cc1 -o %t -w  -g %s
+// RUN: %clang_cc1 -triple i386-apple-darwin10 -fblocks -g -S %s -o -
 
+// rdar://7556129
+@implementation test
+- (void)wait {
+  ^{};
+}
+@end
 
+// PR4894
+@interface I0 {
+  I0 *_iv0;
+}
+@end
+@protocol P0 @end
+
+@interface I1 @end
+@implementation I1
+- (I0<P0> *) im0 {
+  // CHECK: @"\01-[I1 im0]"
+  // CHECK: llvm.dbg.func.start
+  return 0;
+}
+@end
+
+// PR4541
 @class NSString;
 @interface NSAttributedString 
 - (NSString *)string;
@@ -15,5 +38,3 @@
   NSMutableAttributedString *attrStr;
 }
 @end
-
-
