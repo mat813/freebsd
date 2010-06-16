mat813/freebsd

commit 560a34322d4b12d501f9f4df22180094e33f0ef2

Improve style.
git-svn-id: https://svn.freebsd.org/base/head@209129 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/arm/arm/nexus.c
@@ -107,6 +107,7 @@ static devclass_t nexus_devclass;
 static int
 nexus_probe(device_t dev)
 {
+
 	device_quiet(dev);	/* suppress attach message for neatness */
 
 	mem_rman.rm_start = 0;
@@ -116,7 +117,7 @@ nexus_probe(device_t dev)
 	if (rman_init(&mem_rman) || rman_manage_region(&mem_rman, 0, ~0u))
 		panic("nexus_probe mem_rman");
 
-	return (0);
+	return (BUS_PROBE_DEFAULT);
 }
 
 static int

commit 03135fac64663b822287c18be5f4b27c44de5940

Move ARM nexus rman initialization to attach routine.
This fixes a panic, which started to trigger after r209129 cleanup.

Submitted by:	Andrew Turner


git-svn-id: https://svn.freebsd.org/base/head@209232 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/arm/arm/nexus.c
@@ -110,13 +110,6 @@ nexus_probe(device_t dev)
 
 	device_quiet(dev);	/* suppress attach message for neatness */
 
-	mem_rman.rm_start = 0;
-	mem_rman.rm_end = ~0u;
-	mem_rman.rm_type = RMAN_ARRAY;
-	mem_rman.rm_descr = "I/O memory addresses";
-	if (rman_init(&mem_rman) || rman_manage_region(&mem_rman, 0, ~0u))
-		panic("nexus_probe mem_rman");
-
 	return (BUS_PROBE_DEFAULT);
 }
 
@@ -143,6 +136,13 @@ nexus_teardown_intr(device_t dev, device_t child, struct resource *r, void *ih)
 static int
 nexus_attach(device_t dev)
 {
+
+	mem_rman.rm_start = 0;
+	mem_rman.rm_end = ~0u;
+	mem_rman.rm_type = RMAN_ARRAY;
+	mem_rman.rm_descr = "I/O memory addresses";
+	if (rman_init(&mem_rman) || rman_manage_region(&mem_rman, 0, ~0u))
+		panic("nexus_probe mem_rman");
 
 	/*
 	 * First, deal with the children we know about already