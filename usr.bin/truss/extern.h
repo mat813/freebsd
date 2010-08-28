mat813/freebsd

commit 41ef3c45df2ffa39ba91935f293e5c5abbfae3c4

MFtbemd:
Prefer MACHNE_CPUARCH to MACHINE_ARCH in most contexts where you want
to test of all the CPUs of a given family conform.


git-svn-id: https://svn.freebsd.org/base/head@211725 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/truss/Makefile
@@ -2,7 +2,7 @@
 
 NO_WERROR=
 PROG=	truss
-SRCS=	main.c setup.c syscalls.c syscalls.h ioctl.c ${MACHINE_ARCH}-fbsd.c
+SRCS=	main.c setup.c syscalls.c syscalls.h ioctl.c ${MACHINE_CPUARCH}-fbsd.c
 
 CFLAGS+= -I${.CURDIR} -I.
 CLEANFILES= syscalls.master syscalls.h ioctl.c
@@ -19,7 +19,7 @@ syscalls.h:	syscalls.master
 ioctl.c: ${.CURDIR}/../kdump/mkioctls
 	sh ${.CURDIR}/../kdump/mkioctls ${DESTDIR}/usr/include > ${.TARGET}
 
-.if ${MACHINE_ARCH} == "i386"
+.if ${MACHINE_CPUARCH} == "i386"
 SRCS+=	i386-linux.c linux_syscalls.h
 CLEANFILES+=i386l-syscalls.master linux_syscalls.h
 
@@ -31,7 +31,7 @@ linux_syscalls.h:	i386l-syscalls.master
 		${.CURDIR}/i386linux.conf
 .endif
 
-.if ${MACHINE_ARCH} == "amd64"
+.if ${MACHINE_CPUARCH} == "amd64"
 SRCS+=	amd64-linux32.c linux32_syscalls.h
 CLEANFILES+=amd64l32-syscalls.master linux32_syscalls.h
 

cddl/lib/libdtrace/Makefile
@@ -63,11 +63,11 @@ CFLAGS+=	-I${.OBJDIR} -I${.CURDIR} \
 
 #CFLAGS+=	-DYYDEBUG
 
-.if ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "amd64"
+.if ${MACHINE_CPUARCH} == "i386" || ${MACHINE_CPUARCH} == "amd64"
 CFLAGS+=	-I${OPENSOLARIS_SYS_DISTDIR}/uts/intel -DDIS_MEM
 .PATH:		${.CURDIR}/../../../cddl/contrib/opensolaris/lib/libdtrace/i386
 .PATH:		${.CURDIR}/../../../sys/cddl/dev/dtrace/${MACHINE_ARCH}
-.elif ${MACHINE_ARCH} == "sparc64"
+.elif ${MACHINE_CPUARCH} == "sparc64"
 CFLAGS+=	-I${OPENSOLARIS_SYS_DISTDIR}/uts/sparc
 .PATH:		${.CURDIR}/../../../cddl/contrib/opensolaris/lib/libdtrace/sparc
 .else

cddl/lib/libzpool/Makefile
@@ -51,7 +51,7 @@ CFLAGS+=	-I${.CURDIR}/../../../cddl/contrib/opensolaris/lib/libnvpair
 CFLAGS+=	-DWANTS_MUTEX_OWNED
 CFLAGS+=	-I${.CURDIR}/../../../lib/libpthread/thread
 CFLAGS+=	-I${.CURDIR}/../../../lib/libpthread/sys
-CFLAGS+=	-I${.CURDIR}/../../../lib/libthr/arch/${MACHINE_ARCH}/include
+CFLAGS+=	-I${.CURDIR}/../../../lib/libthr/arch/${MACHINE_CPUARCH}/include
 
 DPADD=		${LIBPTHREAD} ${LIBZ}
 LDADD=		-lpthread -lz

gnu/lib/csu/Makefile
@@ -24,18 +24,18 @@ CFLAGS+=	-I${GCCLIB}/include -I${GCCDIR}/config -I${GCCDIR} -I. \
 CRTS_CFLAGS=	-DCRTSTUFFS_O -DSHARED ${PICFLAG}
 MKDEP=		-DCRT_BEGIN
 
-.if ${MACHINE_ARCH} == "ia64"
+.if ${MACHINE_CPUARCH} == "ia64"
 BEGINSRC=	crtbegin.asm
 ENDSRC=		crtend.asm
 CFLAGS+=	-x assembler-with-cpp	# Ugly hack
 CFLAGS+=	-include osreldate.h
 .undef SRCS				# hack for 'make depend'
 .endif
-.if ${MACHINE_ARCH} == "powerpc" || ${MACHINE_ARCH} == "powerpc64"
+.if ${MACHINE_CPUARCH} == "powerpc"
 TGTOBJS=	crtsavres.o
 SRCS+=		crtsavres.asm
 .endif
-.if ${MACHINE_ARCH} == "sparc64"
+.if ${MACHINE_CPUARCH} == "sparc64"
 TGTOBJS=	crtfastmath.o
 SRCS+=		crtfastmath.c
 .endif

gnu/lib/libgomp/Makefile
@@ -23,9 +23,10 @@ CFLAGS+= -I${.CURDIR} -I. -I${SRCDIR} -I${SRCDIR}/config/posix
 VERSION_MAP=	${SRCDIR}/libgomp.map
 
 # Target-specific OpenMP configuration
-.if ${MACHINE_ARCH} == arm || ${MACHINE_ARCH} == i386 || \
-    ${MACHINE_ARCH} == powerpc || \
-    (${MACHINE_ARCH} == mips && (!defined(TARGET_ABI) || ${TARGET_ABI} != "n64"))
+.if ${MACHINE_CPUARCH} == arm || ${MACHINE_CPUARCH} == i386 || \
+    ${MACHINE_CPUARCH} == powerpc || \
+    (${MACHINE_CPUARCH} == mips && \
+	(!defined(TARGET_ABI) || ${TARGET_ABI} != "n64"))
 OMP_LOCK_ALIGN	=	4
 OMP_LOCK_KIND=		4
 OMP_LOCK_SIZE=		4

gnu/lib/libstdc++/Makefile
@@ -14,7 +14,7 @@ LIB=		stdc++
 SHLIB_MAJOR=	6
 
 CFLAGS+=	-DIN_GLIBCPP_V3 -DHAVE_CONFIG_H
-.if ${MACHINE_ARCH} == "arm"
+.if ${MACHINE_CPUARCH} == "arm"
 CFLAGS+=	-D_GLIBCXX_SJLJ_EXCEPTIONS=1
 .endif
 CFLAGS+=	-I${.CURDIR} -I${SUPDIR} -I${GCCDIR} -I${SRCDIR}/include
@@ -67,14 +67,14 @@ SRCS+=	del_op.cc del_opnt.cc del_opv.cc del_opvnt.cc eh_alloc.cc eh_arm.cc \
 SRCS+=	cp-demangle.c
 
 # MD headers location
-.if ${MACHINE_ARCH} == "sparc64"
+.if ${MACHINE_CPUARCH} == "sparc64"
 MARCHDIR=	sparc
-.elif ${MACHINE_ARCH} == "i386" && ${MACHINE_CPU} != 'i386'
+.elif ${MACHINE_CPUARCH} == "i386" && ${MACHINE_CPU} != 'i386'
 MARCHDIR=	i486
-.elif ${MACHINE_ARCH} == "amd64"
+.elif ${MACHINE_CPUARCH} == "amd64"
 MARCHDIR=	i486
 .else
-MARCHDIR=	${MACHINE_ARCH}
+MARCHDIR=	${MACHINE_CPUARCH}
 .endif
 
 .if exists(${SRCDIR}/config/cpu/${MARCHDIR}/atomicity.h)
@@ -83,7 +83,7 @@ ATOMICITY_H=	${SRCDIR}/config/cpu/${MARCHDIR}/atomicity.h
 ATOMICITY_H=	${SRCDIR}/config/cpu/generic/atomicity_mutex/atomicity.h
 .endif
 
-.if ${MACHINE_ARCH} == "amd64"
+.if ${MACHINE_CPUARCH} == "amd64"
 .if exists(${SRCDIR}/config/cpu/generic/atomicity_builtins/atomicity.h)
 ATOMICITY_H=	${SRCDIR}/config/cpu/generic/atomicity_builtins/atomicity.h
 .endif

gnu/usr.bin/binutils/gdb/Makefile
@@ -8,16 +8,7 @@ GDBDIR= ${.CURDIR}/../../../../contrib/gdb
 .PATH: ${SRCDIR}/opcodes ${SRCDIR}/binutils
 
 #	For FSF GDB files, use their CPU (arch) name; for our files use ours.
-.if ${TARGET_ARCH} == "sparc64"
-GDB_CPU=	sparc
-.elif ${TARGET_ARCH} == "amd64"
-GDB_CPU=	i386
-.elif ${TARGET_ARCH} == "powerpc64"
-GDB_CPU=        powerpc
-.else
-GDB_CPU=	${TARGET_ARCH}
-.endif
-
+GDB_CPU=${TARGET_CPUARCH:C/amd64/i386/:C/powerpc.*/rs6000/:C/sparc64/sparc/}
 NO_SHARED?=yes
 PROG=	gdb
 XSRCS=	annotate.c arch-utils.c ax-general.c ax-gdb.c bcache.c		\
@@ -57,7 +48,7 @@ WARNS?=	0
 CFLAGS+=	-DCROSS_COMPILE=1
 .endif
 CFLAGS+=	-DDEFAULT_BFD_ARCH=bfd_${GDB_CPU}_arch
-CFLAGS+=	-I${.CURDIR}/${TARGET_ARCH}
+CFLAGS+=	-I${.CURDIR}/${TARGET_CPUARCH}
 CFLAGS+=	-I${SRCDIR}/binutils -I${SRCDIR}/bfd
 CFLAGS+=	-I${GDBDIR}/gdb -I${GDBDIR}/gdb/config
 CFLAGS+=        -I$(.CURDIR)
@@ -123,8 +114,8 @@ init.c: ${XSRCS}
 
 tm.h:
 	echo '#include "${GDB_CPU}/tm-fbsd.h"'		> ${.TARGET}
-.if exists(${.CURDIR}/fbsd-kgdb-${TARGET_ARCH}.h)
-	echo '#include "fbsd-kgdb-${TARGET_ARCH}.h"'	>> ${.TARGET}
+.if exists(${.CURDIR}/fbsd-kgdb-${TARGET_CPUARCH}.h)
+	echo '#include "fbsd-kgdb-${TARGET_CPUARCH}.h"'	>> ${.TARGET}
 .endif
 
 .for H in nm-fbsd xm-${GDB_CPU}
@@ -133,7 +124,7 @@ ${H:C/-.*$//}.h:
 .endfor
 
 kvm-fbsd-machine.h:
-	ln -sf ${.CURDIR}/kvm-fbsd-${TARGET_ARCH}.h ${.TARGET}
+	ln -sf ${.CURDIR}/kvm-fbsd-${TARGET_CPUARCH}.h ${.TARGET}
 
 GDB_VERSION=	"5.2.1 (FreeBSD)"
 gdbversion.c: Makefile

gnu/usr.bin/gdb/Makefile
@@ -2,8 +2,7 @@
 
 SUBDIR=	doc libgdb gdb gdbtui kgdb
 
-TARGET_ARCH?= ${MACHINE_ARCH}
-.if exists(${.CURDIR}/gdbserver/reg-${TARGET_ARCH}.c)
+.if exists(${.CURDIR}/gdbserver/reg-${MACHINE_CPUARCH}.c)
 SUBDIR+=gdbserver
 .endif
 

commit 09fa353f0f84e0877245d91fbdc2648c6a0edc6c

Fix breakage introduced in r211725 and improve functionality of truss on 64-bit powerpc by adding 32-bit compatibility features.
git-svn-id: https://svn.freebsd.org/base/head@211935 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

usr.bin/truss/Makefile
@@ -2,7 +2,13 @@
 
 NO_WERROR=
 PROG=	truss
-SRCS=	main.c setup.c syscalls.c syscalls.h ioctl.c ${MACHINE_CPUARCH}-fbsd.c
+SRCS=	main.c setup.c syscalls.c syscalls.h ioctl.c
+
+.if exists(${.CURDIR}/${MACHINE_ARCH}-fbsd.c)
+SRCS+= ${MACHINE_ARCH}-fbsd.c
+.else
+SRCS+= ${MACHINE_CPUARCH}-fbsd.c
+.endif
 
 CFLAGS+= -I${.CURDIR} -I.
 CLEANFILES= syscalls.master syscalls.h ioctl.c
@@ -43,6 +49,18 @@ linux32_syscalls.h:	amd64l32-syscalls.master
 		${.CURDIR}/amd64linux32.conf
 
 SRCS+=	amd64-fbsd32.c freebsd32_syscalls.h
+CLEANFILES+=fbsd32-syscalls.master freebsd32_syscalls.h
+
+fbsd32-syscalls.master: ${.CURDIR}/../../sys/compat/freebsd32/syscalls.master
+	cat ${.ALLSRC} > ${.TARGET}
+
+freebsd32_syscalls.h:	fbsd32-syscalls.master
+	/bin/sh ${.CURDIR}/../../sys/kern/makesyscalls.sh ${.ALLSRC} \
+		${.CURDIR}/fbsd32.conf
+.endif
+
+.if ${MACHINE_ARCH} == "powerpc64"
+SRCS+=	powerpc-fbsd.c freebsd32_syscalls.h
 CLEANFILES+=fbsd32-syscalls.master freebsd32_syscalls.h
 
 fbsd32-syscalls.master: ${.CURDIR}/../../sys/compat/freebsd32/syscalls.master

usr.bin/truss/extern.h
@@ -58,6 +58,8 @@ extern long ia64_syscall_exit(struct trussinfo *, int);
 #ifdef __powerpc__
 extern void powerpc_syscall_entry(struct trussinfo *, int);
 extern long powerpc_syscall_exit(struct trussinfo *, int);
+extern void powerpc64_syscall_entry(struct trussinfo *, int);
+extern long powerpc64_syscall_exit(struct trussinfo *, int);
 #endif
 #ifdef __sparc64__
 extern void sparc64_syscall_entry(struct trussinfo *, int);

usr.bin/truss/main.c
@@ -97,6 +97,9 @@ struct ex_types {
 #ifdef __powerpc__
 	{ "FreeBSD ELF", powerpc_syscall_entry, powerpc_syscall_exit },
 	{ "FreeBSD ELF32", powerpc_syscall_entry, powerpc_syscall_exit },
+#ifdef __powerpc64__
+	{ "FreeBSD ELF64", powerpc64_syscall_entry, powerpc64_syscall_exit },
+#endif
 #endif
 #ifdef __sparc64__
 	{ "FreeBSD ELF64", sparc64_syscall_entry, sparc64_syscall_exit },

usr.bin/truss/powerpc-fbsd.c
@@ -63,7 +63,12 @@ static const char rcsid[] =
 
 static int cpid = -1;
 
+#ifdef __powerpc64__	/* 32-bit compatibility */
+#include "freebsd32_syscalls.h"
+#define  syscallnames freebsd32_syscallnames
+#else			/* native 32-bit */
 #include "syscalls.h"
+#endif
 
 static int nsyscalls = sizeof(syscallnames) / sizeof(syscallnames[0]);
 

usr.bin/truss/powerpc64-fbsd.c
@@ -109,7 +109,7 @@ clear_fsc(void) {
  */
 
 void
-powerpc_syscall_entry(struct trussinfo *trussinfo, int nargs) {
+powerpc64_syscall_entry(struct trussinfo *trussinfo, int nargs) {
   char buf[32];
   struct reg regs;
   void *args;
@@ -254,7 +254,7 @@ powerpc_syscall_entry(struct trussinfo *trussinfo, int nargs) {
  */
 
 long
-powerpc_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
+powerpc64_syscall_exit(struct trussinfo *trussinfo, int syscall_num __unused)
 {
   struct reg regs;
   long retval;