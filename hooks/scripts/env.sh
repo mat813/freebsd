40b6f9157f566f146a68e3042ded401db7602179
bz <bz@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2012-09-15 18:26:09 +0000

    As an intermediate step to unify checkacl, check for a base (src) specific version as we do for doc and ports currently.
    Add an extra argument, which is currently ignored for base, stating that
    it is a request for a commit to base.
    
    
    git-svn-id: https://svn.freebsd.org/base/svnadmin@240535 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- hooks/scripts/env.sh
+++ hooks/scripts/env.sh
@@ -4,6 +4,8 @@ PATH=/s/svn/base/hooks/scripts:/usr/bin:/bin:/usr/local/bin:/usr/sbin:/sbin:/usr
 export PATH
 cd /s/svn/base
 umask 002
-if [ -x /usr/local/bin/checkacl ]; then
-  eval `/usr/local/bin/checkacl`
+if [ -x /usr/local/bin/checkacl-base ]; then
+  eval `/usr/local/bin/checkacl-base base`
+elif [ -x /usr/local/bin/checkacl ]; then
+  eval `/usr/local/bin/checkacl base`
 fi
