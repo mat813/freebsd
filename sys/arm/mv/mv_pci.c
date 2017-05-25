mat813/freebsd

commit 6fc4cd908d1f7678b8c522b4aeaf745d2a3b497e

Remove an extra '!' found by clang 3.8.
git-svn-id: https://svn.freebsd.org/base/head@294510 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/arm/mv/mv_pci.c
@@ -922,7 +922,7 @@ static inline void
 pcib_write_irq_mask(struct mv_pcib_softc *sc, uint32_t mask)
 {
 
-	if (!sc->sc_type != MV_TYPE_PCI)
+	if (sc->sc_type != MV_TYPE_PCI)
 		return;
 
 	bus_space_write_4(sc->sc_bst, sc->sc_bsh, PCIE_REG_IRQ_MASK, mask);

commit 0ea01d5b7eb36722905d4b0b9e9bac940b262e68

Unmask legacy interrupts on Marvell PCIE controller
This patch fixes a bug introduced with commit:
r294510  "Remove an extra '!' found by clang 3.8."

'!' was removed without inverting the logic, which
broke PCIe legacy interrupts operation for Marvell
controllers.

Submitted by: Michal Mazur <mkm@semihalf.com>
Obtained from: Semihalf
Sponsored by: Netgate


git-svn-id: https://svn.freebsd.org/base/head@318880 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/arm/mv/mv_pci.c
@@ -918,7 +918,7 @@ static inline void
 pcib_write_irq_mask(struct mv_pcib_softc *sc, uint32_t mask)
 {
 
-	if (sc->sc_type != MV_TYPE_PCI)
+	if (sc->sc_type != MV_TYPE_PCIE)
 		return;
 
 	bus_space_write_4(sc->sc_bst, sc->sc_bsh, PCIE_REG_IRQ_MASK, mask);