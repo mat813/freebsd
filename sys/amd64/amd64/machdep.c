mat813/freebsd

commit 5c9039894ae4638bb73212b0c9dfd3f54c23607e

Detect hypervisors early.  We used to set lower hz on hypervisors by default but it was broken since r273800 (and r278522, its MFC to stable/10) because identify_cpu() is called too late, i.e., after init_param1().
MFC after:	3 days


git-svn-id: https://svn.freebsd.org/base/head@322076 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/amd64/amd64/machdep.c
@@ -1537,6 +1537,8 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 
 	kmdp = init_ops.parse_preload_data(modulep);
 
+	identify_hypervisor();
+
 	/* Init basic tunables, hz etc */
 	init_param1();
 

sys/x86/include/x86_var.h
@@ -116,6 +116,7 @@ void	cpu_setregs(void);
 void	dump_add_page(vm_paddr_t);
 void	dump_drop_page(vm_paddr_t);
 void	identify_cpu(void);
+void	identify_hypervisor(void);
 void	initializecpu(void);
 void	initializecpucache(void);
 bool	fix_cpuid(void);

sys/x86/x86/identcpu.c
@@ -1252,7 +1252,7 @@ static const char *const vm_pnames[] = {
 	NULL
 };
 
-static void
+void
 identify_hypervisor(void)
 {
 	u_int regs[4];
@@ -1403,7 +1403,6 @@ identify_cpu(void)
 	cpu_feature2 = regs[2];
 #endif
 
-	identify_hypervisor();
 	cpu_vendor_id = find_cpu_vendor_id();
 
 	if (fix_cpuid()) {

sys/i386/i386/machdep.c
@@ -2185,6 +2185,8 @@ init386(int first)
 	else
 		init_static_kenv(NULL, 0);
 
+	identify_hypervisor();
+
 	/* Init basic tunables, hz etc */
 	init_param1();
 

commit 1c670977a0b0921b15ec417b895a98e79d8af6d9

Split identify_cpu() into two functions for amd64 as we do for i386.  This reduces diff between amd64 and i386.  Also, it fixes a regression introduced in r322076, i.e., identify_hypervisor() failed to identify some hypervisors. This function assumes cpu_feature2 is already initialized.
Reported by:	dexuan
Tested by:	dexuan


git-svn-id: https://svn.freebsd.org/base/head@322323 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/amd64/amd64/machdep.c
@@ -1537,6 +1537,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 
 	kmdp = init_ops.parse_preload_data(modulep);
 
+	identify_cpu();
 	identify_hypervisor();
 
 	/* Init basic tunables, hz etc */
@@ -1643,7 +1644,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 	    != NULL)
 		vty_set_preferred(VTY_VT);
 
-	identify_cpu();		/* Final stage of CPU initialization */
+	finishidentcpu();	/* Final stage of CPU initialization */
 	initializecpu();	/* Initialize CPU registers */
 	initializecpucache();
 

sys/i386/include/md_var.h
@@ -59,7 +59,6 @@ void	doreti_popl_es(void) __asm(__STRING(doreti_popl_es));
 void	doreti_popl_es_fault(void) __asm(__STRING(doreti_popl_es_fault));
 void	doreti_popl_fs(void) __asm(__STRING(doreti_popl_fs));
 void	doreti_popl_fs_fault(void) __asm(__STRING(doreti_popl_fs_fault));
-void	finishidentcpu(void);
 void	fill_based_sd(struct segment_descriptor *sdp, uint32_t base);
 void	i686_pagezero(void *addr);
 void	sse2_pagezero(void *addr);

sys/x86/include/x86_var.h
@@ -115,6 +115,7 @@ void	cpu_probe_amdc1e(void);
 void	cpu_setregs(void);
 void	dump_add_page(vm_paddr_t);
 void	dump_drop_page(vm_paddr_t);
+void	finishidentcpu(void);
 void	identify_cpu(void);
 void	identify_hypervisor(void);
 void	initializecpu(void);

sys/x86/x86/identcpu.c
@@ -1372,23 +1372,12 @@ fix_cpuid(void)
 	return (false);
 }
 
-/*
- * Final stage of CPU identification.
- */
-#ifdef __i386__
-void
-finishidentcpu(void)
-#else
+#ifdef __amd64__
 void
 identify_cpu(void)
-#endif
 {
-	u_int regs[4], cpu_stdext_disable;
-#ifdef __i386__
-	u_char ccr3;
-#endif
+	u_int regs[4];
 
-#ifdef __amd64__
 	do_cpuid(0, regs);
 	cpu_high = regs[0];
 	((u_int *)&cpu_vendor)[0] = regs[1];
@@ -1401,6 +1390,18 @@ identify_cpu(void)
 	cpu_procinfo = regs[1];
 	cpu_feature = regs[3];
 	cpu_feature2 = regs[2];
+}
+#endif
+
+/*
+ * Final stage of CPU identification.
+ */
+void
+finishidentcpu(void)
+{
+	u_int regs[4], cpu_stdext_disable;
+#ifdef __i386__
+	u_char ccr3;
 #endif
 
 	cpu_vendor_id = find_cpu_vendor_id();