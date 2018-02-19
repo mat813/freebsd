mat813/freebsd

commit 5d0d3d4eac56c0347e5ebcef064d2165b6c3575c

exit: stop doing PROC_SLOCK just to call proc_reap
It immediately does PROC_SUNLOCK anyway and the lock plays no role.


git-svn-id: https://svn.freebsd.org/base/head@329449 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/kern/kern_exit.c
@@ -817,12 +817,10 @@ proc_reap(struct thread *td, struct proc *p, int *status, int options)
 
 	sx_assert(&proctree_lock, SA_XLOCKED);
 	PROC_LOCK_ASSERT(p, MA_OWNED);
-	PROC_SLOCK_ASSERT(p, MA_OWNED);
 	KASSERT(p->p_state == PRS_ZOMBIE, ("proc_reap: !PRS_ZOMBIE"));
 
 	q = td->td_proc;
 
-	PROC_SUNLOCK(p);
 	if (status)
 		*status = KW_EXITCODE(p->p_xexit, p->p_xsig);
 	if (options & WNOWAIT) {
@@ -1090,7 +1088,6 @@ proc_to_reap(struct thread *td, struct proc *p, idtype_t idtype, id_t id,
 	}
 
 	if (p->p_state == PRS_ZOMBIE && !check_only) {
-		PROC_SLOCK(p);
 		proc_reap(td, p, status, options);
 		return (-1);
 	}

commit 01477ffe988dfbb7ee0663d1fc7688d2d6efd0c1

Fix process exit vs reap race introduced in r329449
The race manifested itself mostly in terms of crashes with "spin lock
held too long".

Relevant parts of respective code paths:

exit:				reap:
PROC_LOCK(p);
PROC_SLOCK(p);
p->p_state == PRS_ZOMBIE
PROC_UNLOCK(p);
				PROC_LOCK(p);
/* exit work */
				if (p->p_state == PRS_ZOMBIE) /* true */
					proc_reap()
					free proc
/* more exit work */
PROC_SUNLOCK(p);

Thus a still exiting process is reaped.

Prior to the change the zombie check was followed by slock/sunlock trip
which prevented the problem.

Even code prior to this commit has a bug: the proc is still accessed for
statistic collection purposes. However, the severity is rather small and
the bug may be fixed in a future commit.

Reported by:	many
Tested by:	allanjude


git-svn-id: https://svn.freebsd.org/base/head@329542 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/kern/kern_exit.c
@@ -819,6 +819,8 @@ proc_reap(struct thread *td, struct proc *p, int *status, int options)
 	PROC_LOCK_ASSERT(p, MA_OWNED);
 	KASSERT(p->p_state == PRS_ZOMBIE, ("proc_reap: !PRS_ZOMBIE"));
 
+	mtx_spin_wait_unlocked(&p->p_slock);
+
 	q = td->td_proc;
 
 	if (status)