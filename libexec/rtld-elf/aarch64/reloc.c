mat813/freebsd

commit 04ddcb2609a7fa53d4123be7cfb126e8f8d22fb4

MFC r341738:
  Implement R_AARCH64_TLS_DTPMOD64 and A_AARCH64_TLS_DTPREL64 relocations.
  Although these are slightly obsolete in favor of R_AARCH64_TLSDESC, gcc
  -mtls-dialect=trad still use them.


git-svn-id: https://svn.freebsd.org/base/stable/11@342111 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

libexec/rtld-elf/aarch64/reloc.c
@@ -217,6 +217,8 @@ reloc_plt(Obj_Entry *obj)
 		case R_AARCH64_TLSDESC:
 			reloc_tlsdesc(obj, rela, where);
 			break;
+		case R_AARCH64_NONE:
+			break;
 		default:
 			_rtld_error("Unknown relocation type %u in PLT",
 			    (unsigned int)ELF_R_TYPE(rela->r_info));
@@ -342,6 +344,9 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 		switch (ELF_R_TYPE(rela->r_info)) {
 		case R_AARCH64_ABS64:
 		case R_AARCH64_GLOB_DAT:
+		case R_AARCH64_TLS_TPREL64:
+		case R_AARCH64_TLS_DTPREL64:
+		case R_AARCH64_TLS_DTPMOD64:
 			def = find_symdef(symnum, obj, &defobj, flags, cache,
 			    lockstate);
 			if (def == NULL)
@@ -392,8 +397,23 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 			*where = def->st_value + rela->r_addend +
 			    defobj->tlsoffset;
 			break;
+
+		/*
+		 * !!! BEWARE !!!
+		 * ARM ELF ABI defines TLS_DTPMOD64 as 1029, and TLS_DTPREL64
+		 * as 1028. But actual bfd linker and the glibc RTLD linker
+		 * treats TLS_DTPMOD64 as 1028 and TLS_DTPREL64 1029.
+		 */
+		case R_AARCH64_TLS_DTPREL64: /* efectively is TLS_DTPMOD64 */
+			*where += (Elf_Addr)defobj->tlsindex;
+			break;
+		case R_AARCH64_TLS_DTPMOD64: /* efectively is TLS_DTPREL64 */
+			*where += (Elf_Addr)(def->st_value + rela->r_addend);
+			break;
 		case R_AARCH64_RELATIVE:
 			*where = (Elf_Addr)(obj->relocbase + rela->r_addend);
+			break;
+		case R_AARCH64_NONE:
 			break;
 		default:
 			rtld_printf("%s: Unhandled relocation %lu\n",

commit 2294e9863a805948bcdf4d0b2d35f630b368a0a4

Fix mismerge caused by r342111. This is a direct commit to stable/11.
git-svn-id: https://svn.freebsd.org/base/stable/11@342112 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

libexec/rtld-elf/aarch64/reloc.c
@@ -344,9 +344,6 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 		switch (ELF_R_TYPE(rela->r_info)) {
 		case R_AARCH64_ABS64:
 		case R_AARCH64_GLOB_DAT:
-		case R_AARCH64_TLS_TPREL64:
-		case R_AARCH64_TLS_DTPREL64:
-		case R_AARCH64_TLS_DTPMOD64:
 			def = find_symdef(symnum, obj, &defobj, flags, cache,
 			    lockstate);
 			if (def == NULL)
@@ -405,9 +402,19 @@ reloc_non_plt(Obj_Entry *obj, Obj_Entry *obj_rtld, int flags,
 		 * treats TLS_DTPMOD64 as 1028 and TLS_DTPREL64 1029.
 		 */
 		case R_AARCH64_TLS_DTPREL64: /* efectively is TLS_DTPMOD64 */
+			def = find_symdef(symnum, obj, &defobj, flags, cache,
+			    lockstate);
+			if (def == NULL)
+				return (-1);
+
 			*where += (Elf_Addr)defobj->tlsindex;
 			break;
 		case R_AARCH64_TLS_DTPMOD64: /* efectively is TLS_DTPREL64 */
+			def = find_symdef(symnum, obj, &defobj, flags, cache,
+			    lockstate);
+			if (def == NULL)
+				return (-1);
+
 			*where += (Elf_Addr)(def->st_value + rela->r_addend);
 			break;
 		case R_AARCH64_RELATIVE: