mat813/freebsd

commit 07abf3a79f9e013bfad0bfae072227ec45b97657

Add sysctls to control device side USB identifiers. This makes it possible to change string and numeric vendor and product identifiers, as well as anything else there might be to change for a particular device side template, eg the MAC address.
Reviewed by:	hselasky@
MFC after:	2 weeks
Sponsored by:	The FreeBSD Foundation
Differential Revision:	https://reviews.freebsd.org/D13920


git-svn-id: https://svn.freebsd.org/base/head@328194 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/usb/template/usb_template_audio.c
@@ -2,7 +2,12 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2010 Hans Petter Selasky. All rights reserved.
+ * Copyright (c) 2010 Hans Petter Selasky
+ * Copyright (c) 2018 The FreeBSD Foundation
+ * All rights reserved.
+ *
+ * Portions of this software were developed by Edward Tomasz Napierala
+ * under sponsorship from the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -56,38 +61,32 @@
 #include <dev/usb/usbdi.h>
 #include <dev/usb/usb_core.h>
 #include <dev/usb/usb_cdc.h>
+#include <dev/usb/usb_ioctl.h>
+#include <dev/usb/usb_util.h>
 
 #include <dev/usb/template/usb_template.h>
 #endif			/* USB_GLOBAL_INCLUDE_FILE */
 
 enum {
-	INDEX_AUDIO_LANG,
-	INDEX_AUDIO_MIXER,
-	INDEX_AUDIO_RECORD,
-	INDEX_AUDIO_PLAYBACK,
-	INDEX_AUDIO_PRODUCT,
-	INDEX_AUDIO_MAX,
+	AUDIO_LANG_INDEX,
+	AUDIO_MIXER_INDEX,
+	AUDIO_RECORD_INDEX,
+	AUDIO_PLAYBACK_INDEX,
+	AUDIO_PRODUCT_INDEX,
+	AUDIO_MAX_INDEX,
 };
 
-#define	STRING_AUDIO_PRODUCT \
-  "A\0u\0d\0i\0o\0 \0T\0e\0s\0t\0 \0D\0e\0v\0i\0c\0e"
+#define	AUDIO_DEFAULT_PRODUCT		"Audio Test Device"
+#define	AUDIO_DEFAULT_MIXER		"Mixer interface"
+#define	AUDIO_DEFAULT_RECORD		"Record interface"
+#define	AUDIO_DEFAULT_PLAYBACK		"Playback interface"
 
-#define	STRING_AUDIO_MIXER \
-  "M\0i\0x\0e\0r\0 \0i\0n\0t\0e\0r\0f\0a\0c\0e"
+static struct usb_string_descriptor	audio_mixer;
+static struct usb_string_descriptor	audio_record;
+static struct usb_string_descriptor	audio_playback;
+static struct usb_string_descriptor	audio_product;
 
-#define	STRING_AUDIO_RECORD \
-  "R\0e\0c\0o\0r\0d\0 \0i\0n\0t\0e\0r\0f\0a\0c\0e"
-
-#define	STRING_AUDIO_PLAYBACK \
-  "P\0l\0a\0y\0b\0a\0c\0k\0 \0i\0n\0t\0e\0r\0f\0a\0c\0e"
-
-
-/* make the real string descriptors */
-
-USB_MAKE_STRING_DESC(STRING_AUDIO_MIXER, string_audio_mixer);
-USB_MAKE_STRING_DESC(STRING_AUDIO_RECORD, string_audio_record);
-USB_MAKE_STRING_DESC(STRING_AUDIO_PLAYBACK, string_audio_playback);
-USB_MAKE_STRING_DESC(STRING_AUDIO_PRODUCT, string_audio_product);
+static struct sysctl_ctx_list		audio_ctx_list;
 
 /* prototypes */
 
@@ -204,7 +203,7 @@ static const struct usb_temp_interface_desc audio_iface_0 = {
 	.bInterfaceClass = UICLASS_AUDIO,
 	.bInterfaceSubClass = UISUBCLASS_AUDIOCONTROL,
 	.bInterfaceProtocol = 0,
-	.iInterface = INDEX_AUDIO_MIXER,
+	.iInterface = AUDIO_MIXER_INDEX,
 };
 
 static const uint8_t audio_raw_desc_20[] = {
@@ -262,7 +261,7 @@ static const struct usb_temp_interface_desc audio_iface_1_alt_0 = {
 	.bInterfaceClass = UICLASS_AUDIO,
 	.bInterfaceSubClass = UISUBCLASS_AUDIOSTREAM,
 	.bInterfaceProtocol = 0,
-	.iInterface = INDEX_AUDIO_PLAYBACK,
+	.iInterface = AUDIO_PLAYBACK_INDEX,
 };
 
 static const struct usb_temp_interface_desc audio_iface_1_alt_1 = {
@@ -271,7 +270,7 @@ static const struct usb_temp_interface_desc audio_iface_1_alt_1 = {
 	.bInterfaceClass = UICLASS_AUDIO,
 	.bInterfaceSubClass = UISUBCLASS_AUDIOSTREAM,
 	.bInterfaceProtocol = 0,
-	.iInterface = INDEX_AUDIO_PLAYBACK,
+	.iInterface = AUDIO_PLAYBACK_INDEX,
 	.isAltInterface = 1,		/* this is an alternate setting */
 };
 
@@ -320,7 +319,7 @@ static const struct usb_temp_interface_desc audio_iface_2_alt_0 = {
 	.bInterfaceClass = UICLASS_AUDIO,
 	.bInterfaceSubClass = UISUBCLASS_AUDIOSTREAM,
 	.bInterfaceProtocol = 0,
-	.iInterface = INDEX_AUDIO_RECORD,
+	.iInterface = AUDIO_RECORD_INDEX,
 };
 
 static const struct usb_temp_interface_desc audio_iface_2_alt_1 = {
@@ -329,7 +328,7 @@ static const struct usb_temp_interface_desc audio_iface_2_alt_1 = {
 	.bInterfaceClass = UICLASS_AUDIO,
 	.bInterfaceSubClass = UISUBCLASS_AUDIOSTREAM,
 	.bInterfaceProtocol = 0,
-	.iInterface = INDEX_AUDIO_RECORD,
+	.iInterface = AUDIO_RECORD_INDEX,
 	.isAltInterface = 1,		/* this is an alternate setting */
 };
 
@@ -346,7 +345,7 @@ static const struct usb_temp_config_desc audio_config_desc = {
 	.ppIfaceDesc = audio_interfaces,
 	.bmAttributes = UC_BUS_POWERED,
 	.bMaxPower = 25,		/* 50 mA */
-	.iConfiguration = INDEX_AUDIO_PRODUCT,
+	.iConfiguration = AUDIO_PRODUCT_INDEX,
 };
 
 static const struct usb_temp_config_desc *audio_configs[] = {
@@ -356,7 +355,7 @@ static const struct usb_temp_config_desc *audio_configs[] = {
 
 static usb_temp_get_string_desc_t audio_get_string_desc;
 
-const struct usb_temp_device_desc usb_template_audio = {
+struct usb_temp_device_desc usb_template_audio = {
 	.getStringDesc = &audio_get_string_desc,
 	.ppConfigDesc = audio_configs,
 	.idVendor = USB_TEMPLATE_VENDOR,
@@ -365,9 +364,7 @@ const struct usb_temp_device_desc usb_template_audio = {
 	.bDeviceClass = UDCLASS_COMM,
 	.bDeviceSubClass = 0,
 	.bDeviceProtocol = 0,
-	.iManufacturer = 0,
-	.iProduct = INDEX_AUDIO_PRODUCT,
-	.iSerialNumber = 0,
+	.iProduct = AUDIO_PRODUCT_INDEX,
 };
 
 /*------------------------------------------------------------------------*
@@ -380,12 +377,12 @@ const struct usb_temp_device_desc usb_template_audio = {
 static const void *
 audio_get_string_desc(uint16_t lang_id, uint8_t string_index)
 {
-	static const void *ptr[INDEX_AUDIO_MAX] = {
-		[INDEX_AUDIO_LANG] = &usb_string_lang_en,
-		[INDEX_AUDIO_MIXER] = &string_audio_mixer,
-		[INDEX_AUDIO_RECORD] = &string_audio_record,
-		[INDEX_AUDIO_PLAYBACK] = &string_audio_playback,
-		[INDEX_AUDIO_PRODUCT] = &string_audio_product,
+	static const void *ptr[AUDIO_MAX_INDEX] = {
+		[AUDIO_LANG_INDEX] = &usb_string_lang_en,
+		[AUDIO_MIXER_INDEX] = &audio_mixer,
+		[AUDIO_RECORD_INDEX] = &audio_record,
+		[AUDIO_PLAYBACK_INDEX] = &audio_playback,
+		[AUDIO_PRODUCT_INDEX] = &audio_product,
 	};
 
 	if (string_index == 0) {
@@ -394,8 +391,66 @@ audio_get_string_desc(uint16_t lang_id, uint8_t string_index)
 	if (lang_id != 0x0409) {
 		return (NULL);
 	}
-	if (string_index < INDEX_AUDIO_MAX) {
+	if (string_index < AUDIO_MAX_INDEX) {
 		return (ptr[string_index]);
 	}
 	return (NULL);
 }
+
+static void
+audio_init(void *arg __unused)
+{
+	struct sysctl_oid *parent;
+	char parent_name[3];
+
+	usb_make_str_desc(&audio_mixer, sizeof(audio_mixer),
+	    AUDIO_DEFAULT_MIXER);
+	usb_make_str_desc(&audio_record, sizeof(audio_record),
+	    AUDIO_DEFAULT_RECORD);
+	usb_make_str_desc(&audio_playback, sizeof(audio_playback),
+	    AUDIO_DEFAULT_PLAYBACK);
+	usb_make_str_desc(&audio_product, sizeof(audio_product),
+	    AUDIO_DEFAULT_PRODUCT);
+
+	snprintf(parent_name, sizeof(parent_name), "%d", USB_TEMP_AUDIO);
+	sysctl_ctx_init(&audio_ctx_list);
+
+	parent = SYSCTL_ADD_NODE(&audio_ctx_list,
+	    SYSCTL_STATIC_CHILDREN(_hw_usb_templates), OID_AUTO,
+	    parent_name, CTLFLAG_RW,
+	    0, "USB Audio Interface device side template");
+	SYSCTL_ADD_U16(&audio_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "vendor_id", CTLFLAG_RWTUN, &usb_template_audio.idVendor,
+	    1, "Vendor identifier");
+	SYSCTL_ADD_U16(&audio_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "product_id", CTLFLAG_RWTUN, &usb_template_audio.idProduct,
+	    1, "Product identifier");
+#if 0
+	SYSCTL_ADD_PROC(&audio_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "mixer", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &audio_mixer, sizeof(audio_mixer), usb_temp_sysctl,
+	    "A", "Mixer interface string");
+	SYSCTL_ADD_PROC(&audio_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "record", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &audio_record, sizeof(audio_record), usb_temp_sysctl,
+	    "A", "Record interface string");
+	SYSCTL_ADD_PROC(&audio_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "playback", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &audio_playback, sizeof(audio_playback), usb_temp_sysctl,
+	    "A", "Playback interface string");
+#endif
+	SYSCTL_ADD_PROC(&audio_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "product", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &audio_product, sizeof(audio_product), usb_temp_sysctl,
+	    "A", "Product string");
+}
+
+static void
+audio_uninit(void *arg __unused)
+{
+
+	sysctl_ctx_free(&audio_ctx_list);
+}
+
+SYSINIT(audio_init, SI_SUB_LOCK, SI_ORDER_FIRST, audio_init, NULL);
+SYSUNINIT(audio_init, SI_SUB_LOCK, SI_ORDER_FIRST, audio_uninit, NULL);

sys/dev/usb/template/usb_template_cdce.c
@@ -3,7 +3,11 @@
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
  * Copyright (c) 2007 Hans Petter Selasky <hselasky@FreeBSD.org>
+ * Copyright (c) 2018 The FreeBSD Foundation
  * All rights reserved.
+ *
+ * Portions of this software were developed by Edward Tomasz Napierala
+ * under sponsorship from the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -57,54 +61,41 @@
 #include <dev/usb/usbdi.h>
 #include <dev/usb/usb_core.h>
 #include <dev/usb/usb_cdc.h>
+#include <dev/usb/usb_ioctl.h>
+#include <dev/usb/usb_util.h>
 
 #include <dev/usb/template/usb_template.h>
 #endif			/* USB_GLOBAL_INCLUDE_FILE */
 
 enum {
-	STRING_LANG_INDEX,
-	STRING_MAC_INDEX,
-	STRING_ETH_CONTROL_INDEX,
-	STRING_ETH_DATA_INDEX,
-	STRING_ETH_CONFIG_INDEX,
-	STRING_ETH_VENDOR_INDEX,
-	STRING_ETH_PRODUCT_INDEX,
-	STRING_ETH_SERIAL_INDEX,
-	STRING_ETH_MAX,
+	ETH_LANG_INDEX,
+	ETH_MAC_INDEX,
+	ETH_CONTROL_INDEX,
+	ETH_DATA_INDEX,
+	ETH_CONFIGURATION_INDEX,
+	ETH_MANUFACTURER_INDEX,
+	ETH_PRODUCT_INDEX,
+	ETH_SERIAL_NUMBER_INDEX,
+	ETH_MAX_INDEX,
 };
 
-#define	STRING_MAC \
-  "2\0A\0002\0003\0004\0005\0006\0007\08\09\0A\0B"
+#define	ETH_DEFAULT_MAC			"2A02030405060789AB"
+#define	ETH_DEFAULT_CONTROL		"USB Ethernet Comm Interface"
+#define	ETH_DEFAULT_DATA		"USB Ethernet Data Interface"
+#define	ETH_DEFAULT_CONFIG		"Default Config"
+#define	ETH_DEFAULT_MANUFACTURER	"FreeBSD foundation"
+#define	ETH_DEFAULT_PRODUCT		"USB Ethernet Adapter"
+#define	ETH_DEFAULT_SERIAL_NUMBER	"December 2007"
 
-#define	STRING_ETH_CONTROL \
-  "U\0S\0B\0 \0E\0t\0h\0e\0r\0n\0e\0t\0 " \
-  "\0C\0o\0m\0m\0 \0I\0n\0t\0e\0r\0f\0a\0c\0e"
+static struct usb_string_descriptor	eth_mac;
+static struct usb_string_descriptor	eth_control;
+static struct usb_string_descriptor	eth_data;
+static struct usb_string_descriptor	eth_configuration;
+static struct usb_string_descriptor	eth_manufacturer;
+static struct usb_string_descriptor	eth_product;
+static struct usb_string_descriptor	eth_serial_number;
 
-#define	STRING_ETH_DATA \
-  "U\0S\0B\0 \0E\0t\0h\0e\0r\0n\0e\0t\0 \0D\0a\0t\0a\0 " \
-  "\0I\0n\0t\0e\0r\0f\0a\0c\0e"
-
-#define	STRING_ETH_CONFIG \
-  "D\0e\0f\0a\0u\0l\0t\0 \0c\0o\0n\0f\0i\0g"
-
-#define	STRING_ETH_VENDOR \
-  "F\0r\0e\0e\0B\0S\0D\0 \0f\0o\0u\0n\0d\0a\0t\0i\0o\0n"
-
-#define	STRING_ETH_PRODUCT \
-  "U\0S\0B\0 \0E\0t\0h\0e\0r\0n\0e\0t\0 \0A\0d\0a\0p\0t\0e\0r"
-
-#define	STRING_ETH_SERIAL \
-  "D\0e\0c\0e\0m\0b\0e\0r\0 \0002\0000\0000\0007"
-
-/* make the real string descriptors */
-
-USB_MAKE_STRING_DESC(STRING_MAC, string_mac);
-USB_MAKE_STRING_DESC(STRING_ETH_CONTROL, string_eth_control);
-USB_MAKE_STRING_DESC(STRING_ETH_DATA, string_eth_data);
-USB_MAKE_STRING_DESC(STRING_ETH_CONFIG, string_eth_config);
-USB_MAKE_STRING_DESC(STRING_ETH_VENDOR, string_eth_vendor);
-USB_MAKE_STRING_DESC(STRING_ETH_PRODUCT, string_eth_product);
-USB_MAKE_STRING_DESC(STRING_ETH_SERIAL, string_eth_serial);
+static struct sysctl_ctx_list		eth_ctx_list;
 
 /* prototypes */
 
@@ -130,7 +121,7 @@ static const struct usb_cdc_ethernet_descriptor eth_enf_desc = {
 	.bLength = sizeof(eth_enf_desc),
 	.bDescriptorType = UDESC_CS_INTERFACE,
 	.bDescriptorSubtype = UDESCSUB_CDC_ENF,
-	.iMacAddress = STRING_MAC_INDEX,
+	.iMacAddress = ETH_MAC_INDEX,
 	.bmEthernetStatistics = {0, 0, 0, 0},
 	.wMaxSegmentSize = {0xEA, 0x05},/* 1514 bytes */
 	.wNumberMCFilters = {0, 0},
@@ -191,7 +182,7 @@ static const struct usb_temp_interface_desc eth_control_interface = {
 	.bInterfaceClass = UICLASS_CDC,
 	.bInterfaceSubClass = UISUBCLASS_ETHERNET_NETWORKING_CONTROL_MODEL,
 	.bInterfaceProtocol = 0,
-	.iInterface = STRING_ETH_CONTROL_INDEX,
+	.iInterface = ETH_CONTROL_INDEX,
 };
 
 static const struct usb_temp_endpoint_desc *eth_data_endpoints[] = {
@@ -205,7 +196,7 @@ static const struct usb_temp_interface_desc eth_data_null_interface = {
 	.bInterfaceClass = UICLASS_CDC_DATA,
 	.bInterfaceSubClass = 0,
 	.bInterfaceProtocol = 0,
-	.iInterface = STRING_ETH_DATA_INDEX,
+	.iInterface = ETH_DATA_INDEX,
 };
 
 static const struct usb_temp_interface_desc eth_data_interface = {
@@ -213,7 +204,7 @@ static const struct usb_temp_interface_desc eth_data_interface = {
 	.bInterfaceClass = UICLASS_CDC_DATA,
 	.bInterfaceSubClass = UISUBCLASS_DATA,
 	.bInterfaceProtocol = 0,
-	.iInterface = STRING_ETH_DATA_INDEX,
+	.iInterface = ETH_DATA_INDEX,
 	.isAltInterface = 1,		/* this is an alternate setting */
 };
 
@@ -228,7 +219,7 @@ static const struct usb_temp_config_desc eth_config_desc = {
 	.ppIfaceDesc = eth_interfaces,
 	.bmAttributes = UC_BUS_POWERED,
 	.bMaxPower = 25,		/* 50 mA */
-	.iConfiguration = STRING_ETH_CONFIG_INDEX,
+	.iConfiguration = ETH_CONFIGURATION_INDEX,
 };
 
 static const struct usb_temp_config_desc *eth_configs[] = {
@@ -236,7 +227,7 @@ static const struct usb_temp_config_desc *eth_configs[] = {
 	NULL,
 };
 
-const struct usb_temp_device_desc usb_template_cdce = {
+struct usb_temp_device_desc usb_template_cdce = {
 	.getStringDesc = &eth_get_string_desc,
 	.ppConfigDesc = eth_configs,
 	.idVendor = USB_TEMPLATE_VENDOR,
@@ -245,9 +236,9 @@ const struct usb_temp_device_desc usb_template_cdce = {
 	.bDeviceClass = UDCLASS_COMM,
 	.bDeviceSubClass = 0,
 	.bDeviceProtocol = 0,
-	.iManufacturer = STRING_ETH_VENDOR_INDEX,
-	.iProduct = STRING_ETH_PRODUCT_INDEX,
-	.iSerialNumber = STRING_ETH_SERIAL_INDEX,
+	.iManufacturer = ETH_MANUFACTURER_INDEX,
+	.iProduct = ETH_PRODUCT_INDEX,
+	.iSerialNumber = ETH_SERIAL_NUMBER_INDEX,
 };
 
 /*------------------------------------------------------------------------*
@@ -260,15 +251,15 @@ const struct usb_temp_device_desc usb_template_cdce = {
 static const void *
 eth_get_string_desc(uint16_t lang_id, uint8_t string_index)
 {
-	static const void *ptr[STRING_ETH_MAX] = {
-		[STRING_LANG_INDEX] = &usb_string_lang_en,
-		[STRING_MAC_INDEX] = &string_mac,
-		[STRING_ETH_CONTROL_INDEX] = &string_eth_control,
-		[STRING_ETH_DATA_INDEX] = &string_eth_data,
-		[STRING_ETH_CONFIG_INDEX] = &string_eth_config,
-		[STRING_ETH_VENDOR_INDEX] = &string_eth_vendor,
-		[STRING_ETH_PRODUCT_INDEX] = &string_eth_product,
-		[STRING_ETH_SERIAL_INDEX] = &string_eth_serial,
+	static const void *ptr[ETH_MAX_INDEX] = {
+		[ETH_LANG_INDEX] = &usb_string_lang_en,
+		[ETH_MAC_INDEX] = &eth_mac,
+		[ETH_CONTROL_INDEX] = &eth_control,
+		[ETH_DATA_INDEX] = &eth_data,
+		[ETH_CONFIGURATION_INDEX] = &eth_configuration,
+		[ETH_MANUFACTURER_INDEX] = &eth_manufacturer,
+		[ETH_PRODUCT_INDEX] = &eth_product,
+		[ETH_SERIAL_NUMBER_INDEX] = &eth_serial_number,
 	};
 
 	if (string_index == 0) {
@@ -277,8 +268,84 @@ eth_get_string_desc(uint16_t lang_id, uint8_t string_index)
 	if (lang_id != 0x0409) {
 		return (NULL);
 	}
-	if (string_index < STRING_ETH_MAX) {
+	if (string_index < ETH_MAX_INDEX) {
 		return (ptr[string_index]);
 	}
 	return (NULL);
 }
+
+static void
+eth_init(void *arg __unused)
+{
+	struct sysctl_oid *parent;
+	char parent_name[3];
+
+	usb_make_str_desc(&eth_mac, sizeof(eth_mac),
+	    ETH_DEFAULT_MAC);
+	usb_make_str_desc(&eth_control, sizeof(eth_control),
+	    ETH_DEFAULT_CONTROL);
+	usb_make_str_desc(&eth_data, sizeof(eth_data),
+	    ETH_DEFAULT_DATA);
+	usb_make_str_desc(&eth_configuration, sizeof(eth_configuration),
+	    ETH_DEFAULT_CONFIG);
+	usb_make_str_desc(&eth_manufacturer, sizeof(eth_manufacturer),
+	    ETH_DEFAULT_MANUFACTURER);
+	usb_make_str_desc(&eth_product, sizeof(eth_product),
+	    ETH_DEFAULT_PRODUCT);
+	usb_make_str_desc(&eth_serial_number, sizeof(eth_serial_number),
+	    ETH_DEFAULT_SERIAL_NUMBER);
+
+	snprintf(parent_name, sizeof(parent_name), "%d", USB_TEMP_CDCE);
+	sysctl_ctx_init(&eth_ctx_list);
+
+	parent = SYSCTL_ADD_NODE(&eth_ctx_list,
+	    SYSCTL_STATIC_CHILDREN(_hw_usb_templates), OID_AUTO,
+	    parent_name, CTLFLAG_RW,
+	    0, "USB CDC Ethernet device side template");
+	SYSCTL_ADD_U16(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "vendor_id", CTLFLAG_RWTUN,
+	    &usb_template_cdce.idVendor, 1, "Vendor identifier");
+	SYSCTL_ADD_U16(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "product_id", CTLFLAG_RWTUN,
+	    &usb_template_cdce.idProduct, 1, "Product identifier");
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "mac", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_mac, sizeof(eth_mac), usb_temp_sysctl,
+	    "A", "MAC address string");
+#if 0
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "control", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_control, sizeof(eth_control), usb_temp_sysctl,
+	    "A", "Control interface string");
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "data", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_data, sizeof(eth_data), usb_temp_sysctl,
+	    "A", "Data interface string");
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "configuration", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_configuration, sizeof(eth_configuration), usb_temp_sysctl,
+	    "A", "Configuration string");
+#endif
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "manufacturer", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_manufacturer, sizeof(eth_manufacturer), usb_temp_sysctl,
+	    "A", "Manufacturer string");
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "product", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_product, sizeof(eth_product), usb_temp_sysctl,
+	    "A", "Product string");
+	SYSCTL_ADD_PROC(&eth_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "serial_number", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &eth_serial_number, sizeof(eth_serial_number), usb_temp_sysctl,
+	    "A", "Serial number string");
+}
+
+static void
+eth_uninit(void *arg __unused)
+{
+
+	sysctl_ctx_free(&eth_ctx_list);
+}
+
+SYSINIT(eth_init, SI_SUB_LOCK, SI_ORDER_FIRST, eth_init, NULL);
+SYSUNINIT(eth_init, SI_SUB_LOCK, SI_ORDER_FIRST, eth_uninit, NULL);

sys/dev/usb/template/usb_template_kbd.c
@@ -2,7 +2,12 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 2010 Hans Petter Selasky. All rights reserved.
+ * Copyright (c) 2010 Hans Petter Selasky
+ * Copyright (c) 2018 The FreeBSD Foundation
+ * All rights reserved.
+ *
+ * Portions of this software were developed by Edward Tomasz Napierala
+ * under sponsorship from the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -56,27 +61,26 @@
 #include <dev/usb/usbdi.h>
 #include <dev/usb/usb_core.h>
 #include <dev/usb/usb_cdc.h>
+#include <dev/usb/usb_ioctl.h>
+#include <dev/usb/usb_util.h>
 
 #include <dev/usb/template/usb_template.h>
 #endif			/* USB_GLOBAL_INCLUDE_FILE */
 
 enum {
-	INDEX_LANG,
-	INDEX_KEYBOARD,
-	INDEX_PRODUCT,
-	INDEX_MAX,
+	KBD_LANG_INDEX,
+	KBD_INTERFACE_INDEX,
+	KBD_PRODUCT_INDEX,
+	KBD_MAX_INDEX,
 };
 
-#define	STRING_PRODUCT \
-  "K\0e\0y\0b\0o\0a\0r\0d\0 \0T\0e\0s\0t\0 \0D\0e\0v\0i\0c\0e"
+#define	KBD_DEFAULT_INTERFACE		"Keyboard Interface"
+#define	KBD_DEFAULT_PRODUCT		"Keyboard Test Device"
 
-#define	STRING_KEYBOARD \
-  "K\0e\0y\0b\0o\0a\0r\0d\0 \0i\0n\0t\0e\0r\0f\0a\0c\0e"
+static struct usb_string_descriptor	kbd_interface;
+static struct usb_string_descriptor	kbd_product;
 
-/* make the real string descriptors */
-
-USB_MAKE_STRING_DESC(STRING_KEYBOARD, string_keyboard);
-USB_MAKE_STRING_DESC(STRING_PRODUCT, string_product);
+static struct sysctl_ctx_list		kbd_ctx_list;
 
 /* prototypes */
 
@@ -135,7 +139,7 @@ static const struct usb_temp_interface_desc keyboard_iface_0 = {
 	.bInterfaceClass = UICLASS_HID,
 	.bInterfaceSubClass = UISUBCLASS_BOOT,
 	.bInterfaceProtocol = UIPROTO_BOOT_KEYBOARD,
-	.iInterface = INDEX_KEYBOARD,
+	.iInterface = KBD_INTERFACE_INDEX,
 };
 
 static const struct usb_temp_interface_desc *keyboard_interfaces[] = {
@@ -147,7 +151,7 @@ static const struct usb_temp_config_desc keyboard_config_desc = {
 	.ppIfaceDesc = keyboard_interfaces,
 	.bmAttributes = UC_BUS_POWERED,
 	.bMaxPower = 25,		/* 50 mA */
-	.iConfiguration = INDEX_PRODUCT,
+	.iConfiguration = KBD_PRODUCT_INDEX,
 };
 
 static const struct usb_temp_config_desc *keyboard_configs[] = {
@@ -158,7 +162,7 @@ static const struct usb_temp_config_desc *keyboard_configs[] = {
 static usb_temp_get_string_desc_t keyboard_get_string_desc;
 static usb_temp_get_vendor_desc_t keyboard_get_vendor_desc;
 
-const struct usb_temp_device_desc usb_template_kbd = {
+struct usb_temp_device_desc usb_template_kbd = {
 	.getStringDesc = &keyboard_get_string_desc,
 	.getVendorDesc = &keyboard_get_vendor_desc,
 	.ppConfigDesc = keyboard_configs,
@@ -169,7 +173,7 @@ const struct usb_temp_device_desc usb_template_kbd = {
 	.bDeviceSubClass = 0,
 	.bDeviceProtocol = 0,
 	.iManufacturer = 0,
-	.iProduct = INDEX_PRODUCT,
+	.iProduct = KBD_PRODUCT_INDEX,
 	.iSerialNumber = 0,
 };
 
@@ -203,10 +207,10 @@ keyboard_get_vendor_desc(const struct usb_device_request *req, uint16_t *plen)
 static const void *
 keyboard_get_string_desc(uint16_t lang_id, uint8_t string_index)
 {
-	static const void *ptr[INDEX_MAX] = {
-		[INDEX_LANG] = &usb_string_lang_en,
-		[INDEX_KEYBOARD] = &string_keyboard,
-		[INDEX_PRODUCT] = &string_product,
+	static const void *ptr[KBD_MAX_INDEX] = {
+		[KBD_LANG_INDEX] = &usb_string_lang_en,
+		[KBD_INTERFACE_INDEX] = &kbd_interface,
+		[KBD_PRODUCT_INDEX] = &kbd_product,
 	};
 
 	if (string_index == 0) {
@@ -215,8 +219,54 @@ keyboard_get_string_desc(uint16_t lang_id, uint8_t string_index)
 	if (lang_id != 0x0409) {
 		return (NULL);
 	}
-	if (string_index < INDEX_MAX) {
+	if (string_index < KBD_MAX_INDEX) {
 		return (ptr[string_index]);
 	}
 	return (NULL);
 }
+
+static void
+kbd_init(void *arg __unused)
+{
+	struct sysctl_oid *parent;
+	char parent_name[3];
+
+	usb_make_str_desc(&kbd_interface, sizeof(kbd_interface),
+	    KBD_DEFAULT_INTERFACE);
+	usb_make_str_desc(&kbd_product, sizeof(kbd_product),
+	    KBD_DEFAULT_PRODUCT);
+
+	snprintf(parent_name, sizeof(parent_name), "%d", USB_TEMP_KBD);
+	sysctl_ctx_init(&kbd_ctx_list);
+
+	parent = SYSCTL_ADD_NODE(&kbd_ctx_list,
+	    SYSCTL_STATIC_CHILDREN(_hw_usb_templates), OID_AUTO,
+	    parent_name, CTLFLAG_RW,
+	    0, "USB Keyboard device side template");
+	SYSCTL_ADD_U16(&kbd_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "vendor_id", CTLFLAG_RWTUN,
+	    &usb_template_kbd.idVendor, 1, "Vendor identifier");
+	SYSCTL_ADD_U16(&kbd_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "product_id", CTLFLAG_RWTUN,
+	    &usb_template_kbd.idProduct, 1, "Product identifier");
+#if 0
+	SYSCTL_ADD_PROC(&kbd_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "interface", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &kbd_interface, sizeof(kbd_interface), usb_temp_sysctl,
+	    "A", "Interface string");
+#endif
+	SYSCTL_ADD_PROC(&kbd_ctx_list, SYSCTL_CHILDREN(parent), OID_AUTO,
+	    "product", CTLTYPE_STRING | CTLFLAG_RWTUN | CTLFLAG_MPSAFE,
+	    &kbd_product, sizeof(kbd_product), usb_temp_sysctl,
+	    "A", "Product string");
+}
+
+static void
+kbd_uninit(void *arg __unused)
+{
+
+	sysctl_ctx_free(&kbd_ctx_list);
+}
+
+SYSINIT(kbd_init, SI_SUB_LOCK, SI_ORDER_FIRST, kbd_init, NULL);
+SYSUNINIT(kbd_init, SI_SUB_LOCK, SI_ORDER_FIRST, kbd_uninit, NULL);

commit 2d5126f0e9e390ff96c273bec2e57604c6d186f4

MFC r328985: Give USB template SYSUNINIT()'s a uniq name to avoid symbol name collision when building stand/usb. Regression after r328194.
Sponsored by:	Mellanox Technologies


git-svn-id: https://svn.freebsd.org/base/stable/11@357434 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/usb/template/usb_template_audio.c
@@ -473,4 +473,4 @@ audio_uninit(void *arg __unused)
 }
 
 SYSINIT(audio_init, SI_SUB_LOCK, SI_ORDER_FIRST, audio_init, NULL);
-SYSUNINIT(audio_init, SI_SUB_LOCK, SI_ORDER_FIRST, audio_uninit, NULL);
+SYSUNINIT(audio_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, audio_uninit, NULL);

sys/dev/usb/template/usb_template_cdce.c
@@ -346,4 +346,4 @@ eth_uninit(void *arg __unused)
 }
 
 SYSINIT(eth_init, SI_SUB_LOCK, SI_ORDER_FIRST, eth_init, NULL);
-SYSUNINIT(eth_init, SI_SUB_LOCK, SI_ORDER_FIRST, eth_uninit, NULL);
+SYSUNINIT(eth_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, eth_uninit, NULL);

sys/dev/usb/template/usb_template_kbd.c
@@ -287,4 +287,4 @@ kbd_uninit(void *arg __unused)
 }
 
 SYSINIT(kbd_init, SI_SUB_LOCK, SI_ORDER_FIRST, kbd_init, NULL);
-SYSUNINIT(kbd_init, SI_SUB_LOCK, SI_ORDER_FIRST, kbd_uninit, NULL);
+SYSUNINIT(kbd_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, kbd_uninit, NULL);

sys/dev/usb/template/usb_template_midi.c
@@ -309,4 +309,4 @@ midi_uninit(void *arg __unused)
 }
 
 SYSINIT(midi_init, SI_SUB_LOCK, SI_ORDER_FIRST, midi_init, NULL);
-SYSUNINIT(midi_init, SI_SUB_LOCK, SI_ORDER_FIRST, midi_uninit, NULL);
+SYSUNINIT(midi_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, midi_uninit, NULL);

sys/dev/usb/template/usb_template_modem.c
@@ -315,4 +315,4 @@ modem_uninit(void *arg __unused)
 }
 
 SYSINIT(modem_init, SI_SUB_LOCK, SI_ORDER_FIRST, modem_init, NULL);
-SYSUNINIT(modem_init, SI_SUB_LOCK, SI_ORDER_FIRST, modem_uninit, NULL);
+SYSUNINIT(modem_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, modem_uninit, NULL);

sys/dev/usb/template/usb_template_mouse.c
@@ -285,4 +285,4 @@ mouse_uninit(void *arg __unused)
 }
 
 SYSINIT(mouse_init, SI_SUB_LOCK, SI_ORDER_FIRST, mouse_init, NULL);
-SYSUNINIT(mouse_init, SI_SUB_LOCK, SI_ORDER_FIRST, mouse_uninit, NULL);
+SYSUNINIT(mouse_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, mouse_uninit, NULL);

sys/dev/usb/template/usb_template_msc.c
@@ -255,4 +255,4 @@ msc_uninit(void *arg __unused)
 }
 
 SYSINIT(msc_init, SI_SUB_LOCK, SI_ORDER_FIRST, msc_init, NULL);
-SYSUNINIT(msc_init, SI_SUB_LOCK, SI_ORDER_FIRST, msc_uninit, NULL);
+SYSUNINIT(msc_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, msc_uninit, NULL);

sys/dev/usb/template/usb_template_mtp.c
@@ -322,4 +322,4 @@ mtp_uninit(void *arg __unused)
 }
 
 SYSINIT(mtp_init, SI_SUB_LOCK, SI_ORDER_FIRST, mtp_init, NULL);
-SYSUNINIT(mtp_init, SI_SUB_LOCK, SI_ORDER_FIRST, mtp_uninit, NULL);
+SYSUNINIT(mtp_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, mtp_uninit, NULL);

sys/dev/usb/template/usb_template_phone.c
@@ -500,4 +500,4 @@ phone_uninit(void *arg __unused)
 }
 
 SYSINIT(phone_init, SI_SUB_LOCK, SI_ORDER_FIRST, phone_init, NULL);
-SYSUNINIT(phone_init, SI_SUB_LOCK, SI_ORDER_FIRST, phone_uninit, NULL);
+SYSUNINIT(phone_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, phone_uninit, NULL);

sys/dev/usb/template/usb_template_serialnet.c
@@ -456,4 +456,4 @@ serialnet_uninit(void *arg __unused)
 }
 
 SYSINIT(serialnet_init, SI_SUB_LOCK, SI_ORDER_FIRST, serialnet_init, NULL);
-SYSUNINIT(serialnet_init, SI_SUB_LOCK, SI_ORDER_FIRST, serialnet_uninit, NULL);
+SYSUNINIT(serialnet_uninit, SI_SUB_LOCK, SI_ORDER_FIRST, serialnet_uninit, NULL);