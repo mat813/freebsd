mat813/freebsd

commit e1b163246c11411ada5e901299da73bc9d364b0e

usb_hub: giving up port reset - device vanished
Improve the output of the recently often experienced debug message in order
to gather further data.

PR:		237666
Reviewed by:	hselasky
MFC after:	3 days
Differential Revision:	https://reviews.freebsd.org/D27108


git-svn-id: https://svn.freebsd.org/base/head@367435 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/usb/usb_hub.c
@@ -720,8 +720,10 @@ repeat:
 		if ((sc->sc_st.port_change & UPS_C_CONNECT_STATUS) ||
 		    (!(sc->sc_st.port_status & UPS_CURRENT_CONNECT_STATUS))) {
 			if (timeout) {
-				DPRINTFN(0, "giving up port reset "
-				    "- device vanished\n");
+			       DPRINTFN(0, "giving up port %d reset - "
+				   "device vanished: change %#x status %#x\n",
+				   portno, sc->sc_st.port_change,
+				   sc->sc_st.port_status);
 				goto error;
 			}
 			timeout = 1;

commit 090ea9fd6ce6e251da4a361f3b4a4df5c2abf742

usb_hub: fix whitespace
Fix a whitespace "error" introduced in r367435 noticed when
preparing the MFC.  No functional changes.


git-svn-id: https://svn.freebsd.org/base/head@367539 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sys/dev/usb/usb_hub.c
@@ -720,7 +720,7 @@ repeat:
 		if ((sc->sc_st.port_change & UPS_C_CONNECT_STATUS) ||
 		    (!(sc->sc_st.port_status & UPS_CURRENT_CONNECT_STATUS))) {
 			if (timeout) {
-			       DPRINTFN(0, "giving up port %d reset - "
+				DPRINTFN(0, "giving up port %d reset - "
 				   "device vanished: change %#x status %#x\n",
 				   portno, sc->sc_st.port_change,
 				   sc->sc_st.port_status);